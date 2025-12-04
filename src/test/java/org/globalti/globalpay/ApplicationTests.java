4b2e02c613d321085d06ec7f08c9a42a2b5bbe74
mathiaspedro9 <mathiaspedro9@gmail.com>
2025-12-04 19:36:02 +0000

    criando a estrutura inicial do microsserviço springboot

---
+++ src/test/java/org/globalti/globalpay/ApplicationTests.java
@@ -0,0 +1,11 @@
+package org.globalti.globalpay;
+
+// import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+	// @Test
+	// void contextLoads() {
+	// }
+}
