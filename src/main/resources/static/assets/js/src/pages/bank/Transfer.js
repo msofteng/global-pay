d893ae2df364c9099edc63980c4b36e38f3ee09a
mathiaspedro9 <mathiaspedro9@gmail.com>
2025-12-06 19:17:31 +0000

    finalizando a integração das transferências bancárias ao app bank web

--- src/main/resources/static/assets/js/src/pages/bank/Transfer.js
+++ src/main/resources/static/assets/js/src/pages/bank/Transfer.js
@@ -1,14 +1,38 @@
 import formDataToJson from "../../shared/functions/form.js";
+import serviceBank from "../../shared/services/bank.js";
 
 export default {
   template: "#transfer-template",
+  data() {
+    return {
+      errors: [],
+      numeroContaOrigem: 0
+    };
+  },
+  mounted() {
+    const usuario = JSON.parse(localStorage.getItem("usuario"));
+    
+    if (usuario) {
+      this.numeroContaOrigem = usuario.numeroConta;
+    } else {
+      window.location.href = "/#/login";
+    }
+  },
   methods: {
-    handleSubmit: (e) => {
+    handleSubmit(e) {
       e.preventDefault();
       
       const data = formDataToJson(new FormData(e.target));
       
-      console.log(data);
+      serviceBank.realizarTransferencia(
+        data
+      ).then(data => {
+        this.errors = [];
+        e.target.reset();
+        window.location.href = "/#/bank";
+      }).catch(error => {
+        this.errors = error.errors;
+      });
     }
   }
 };
