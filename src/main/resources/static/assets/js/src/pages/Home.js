5eec430c067a1c1af349dc395b7f0af773504071
mathiaspedro9 <mathiaspedro9@gmail.com>
2025-12-06 09:03:32 +0000

    adicionando o tema da globalpay no website

--- src/main/resources/static/assets/js/src/pages/Home.js
+++ src/main/resources/static/assets/js/src/pages/Home.js
@@ -8,8 +8,8 @@ export default {
     function showSlide(i) {
       slides.forEach((slide, idx) => {
         slide.style.opacity = idx === i ? "1" : "0";
-        dots[idx].classList.toggle("bg-gray-300", idx === i);
-        dots[idx].classList.toggle("bg-gray-500", idx !== i);
+        dots[idx].classList.toggle("bg-brand", idx === i);
+        dots[idx].classList.toggle("bg-primary", idx !== i);
       });
     }
 
@@ -31,7 +31,28 @@ export default {
       showSlide(index);
     });
 
-    // Auto-play
-    setInterval(nextSlide, 5000);
+    setInterval(nextSlide, 3000);
+  },
+  data() {
+    return {
+      slides: [
+        {
+          title: `<br><br>40 anos de GlobalPay`,
+          image: 'assets/img/posts/f-1.png'
+        },
+        {
+          title: `Faça parte de uma equipe que vem crescendo ano após ano`,
+          image: 'assets/img/posts/b-1.png'
+        },
+        {
+          title: `<p class="text-shadow-lg text-shadow-black/50">FAB realiza homenagem para a GlobalPay</p>`,
+          image: 'assets/img/posts/k-2.png'
+        },
+        {
+          title: `<i class="text-white text-shadow-lg text-shadow-black/50">Nosso time feminino da Global!!!</i>`,
+          image: `assets/img/posts/e-2.png`
+        }
+      ]
+    }
   }
 };
